ed81f6c9b26eb65973037286b7e619b11cded146
b6110401609 <chaimongkon.p@ku.th>
2020-09-27 20:45:12 +0700

    Spring JPA

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 public class CustomerService {
     private CustomerRepository repository;
@@ -21,10 +23,11 @@ public class CustomerService {
     }
     public Customer findCustomer(int id) {
         try {
-            return repository.findById(id);
-        } catch (EmptyResultDataAccessException e) {
+            return repository.findById(id).get();
+        } catch (NoSuchElementException e) {
             return null;
         }
+
     }
     public Customer checkPin(Customer inputCustomer) {
         Customer storedCustomer = findCustomer(inputCustomer.getId());
